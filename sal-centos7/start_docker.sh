9b621b3dd6078429982a22eaa59806cc3facccd6
Felipe Menanteau <felipe@illinois.edu>
2019-06-14 14:57:42 -0500

    Update to Dockerfile

--- sal-centos7/start_docker.sh
+++ sal-centos7/start_docker.sh
@@ -1,4 +1,4 @@
-HSUSER=felipe
+HSUSER=headerservice
 DOCKER_IMA=centos7_sal
 hostname="`hostname -s`-docker"
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
@@ -8,5 +8,5 @@ echo "starting docker with image: $DOCKER_IMA"
 docker run -ti -e DISPLAY=$ip:0 \
     -h $hostname \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /Users/felipe/sal-home:/home/$HSUSER \
+    -v $HOME/sal-home:/home/$HSUSER \
     $DOCKER_IMA bash
